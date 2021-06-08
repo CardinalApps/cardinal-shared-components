580ac4116c39489ecf629877db9cb0316363c931
somebeaver <admin@somebeaver.ca>
2021-06-08 19:44:23 -0400

    fixed console message colors

--- app-base/console-message.js
+++ app-base/console-message.js
@@ -16,6 +16,7 @@ export default function consoleMessage() {
   console.log('%cOrange: Main process announcements', 'color:#f39a11;')
   console.log('%cGreen: Bridge object instance', 'color:#5fb64a;')
   console.log('%cBlue: UI Router object instance', 'color:#47aac9;')
+  console.log('%cPink: Audio playback', 'color:#d61fd0;')
   console.log('Default: Other')
   console.groupEnd()
 }
